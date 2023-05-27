84ecb5f5248b2c57263db784909e32883a652db0
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-05-27 21:28:00 +0300

    Create functions to get and send data

---
+++ src/utils/fetch.ts
@@ -0,0 +1,21 @@
+export const getData = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Failed to get data')
+  }
+  return await response.json();
+};
+
+export const sendData = async (url: string, data: any) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  if (!response.ok) {
+    throw new Error('Failed to send data')
+  }
+  return await response.json();
+}
