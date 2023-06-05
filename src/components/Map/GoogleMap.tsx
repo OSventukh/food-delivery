d910505944eb8d72593e133179caddee2f3f82ca
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-06-05 22:30:52 +0300

    Add route between restaurant and customer

--- src/components/Map/GoogleMap.tsx
+++ src/components/Map/GoogleMap.tsx
@@ -17,6 +17,9 @@ export default function GoogleMap({
 }) {
   const [map, setMap] = useState<google.maps.Map>();
   const mapRef = useRef<HTMLDivElement>(null);
+  const infoWindowRef = useRef<google.maps.InfoWindow>();
+  const directionsServiceRef = useRef<google.maps.DirectionsService>();
+  const directionsRendererRef = useRef<google.maps.DirectionsRenderer>();
 
   useEffect(() => {
     if (mapRef.current && !map) {
@@ -30,13 +33,80 @@ export default function GoogleMap({
   }, [center, zoom, map]);
 
   useEffect(() => {
+    if (markersPosition && markersPosition.length > 1) {
+      if (!directionsServiceRef.current) {
+        directionsServiceRef.current = new google.maps.DirectionsService();
+      }
+
+      if (!directionsRendererRef.current) {
+        directionsRendererRef.current = new google.maps.DirectionsRenderer({
+          suppressMarkers: true,
+        });
+      }
+
+
+      directionsRendererRef.current.setMap(map ? map : null);
+
+      const waypoints = markersPosition.map((position) => ({
+        location: position,
+        stopover: true,
+      }));
+
+      directionsServiceRef.current.route(
+        {
+          origin: markersPosition[0],
+          destination: markersPosition[markersPosition.length - 1],
+          waypoints,
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsRendererRef.current?.setDirections(result);
+            // getting the travel time
+            const route = result?.routes[0];
+
+            if (route) {
+              const duration = route.legs.reduce(
+                (total, leg) => total + leg!.duration!.value,
+                0
+              );
+              const durationInMinutes = Math.round(duration / 60);
+
+              if (infoWindowRef.current) {
+                infoWindowRef.current.close();
+              }
+
+              const infoWindow = new google.maps.InfoWindow({
+                disableAutoPan: true,
+              });
+
+              // display the travel time on the map
+              infoWindow.setContent(
+                `Estimated travel time: ${durationInMinutes} minutes`
+              );
+              infoWindow.setPosition(
+                route.legs[route.legs.length - 1].end_location
+              );
+              infoWindow.open(map);
+              
+              infoWindowRef.current = infoWindow;
+            }
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    }
+  }, [map, markersPosition]);
+
+  useEffect(() => {
     // map positioning relative to markers
     if (markersPosition && markersPosition.length > 0) {
       const bounds = new google.maps.LatLngBounds();
 
       markersPosition.forEach((item) => {
         bounds.extend(item);
-      })
+      });
 
       map?.fitBounds(bounds);
     }
